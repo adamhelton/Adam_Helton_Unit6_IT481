a458ac76823e167916b4ac9cb7665187fb3eb297
Adam Helton <helton93@protonmail.com>
2020-09-04 12:45:05 -0400

    Added semaphores to DressingRooms.cs

--- Adam_Helton_Unit6_IT481/DressingRooms.cs
+++ Adam_Helton_Unit6_IT481/DressingRooms.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
 namespace Adam_Helton_Unit6_IT481
 {
     public class DressingRooms
     {
-        
+        private int rooms;
+        private Semaphore semaphore;
+        private long waitTimer;
+        private long runTimer;
+
+        public DressingRooms()
+        {
+            rooms = 3;
+            semaphore = new Semaphore(rooms, rooms);
+        }
+
+        public DressingRooms(int r)
+        {
+            rooms = r;
+            semaphore = new Semaphore(rooms, rooms);
+        }
     }
 }
